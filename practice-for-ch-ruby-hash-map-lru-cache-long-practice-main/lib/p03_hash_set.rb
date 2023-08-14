64c1fa41afcfcff924d4ac75259ff02e4c27aa35
David <2018dpollack@gmail.com>
2023-08-14 16:22:21 -0700

    finish hash_set

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p03_hash_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p03_hash_set.rb
@@ -7,12 +7,23 @@ class HashSet
   end
 
   def insert(key)
+    self.resize! if self.count == num_buckets
+    unless self[key].include?(key)
+      self[key] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    return true if self[key].include?(key)
+    false
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
@@ -22,9 +33,17 @@ class HashSet
   end
 
   def resize!
+    copy = @store.dup
+    @store = Array.new(@count * 2) {Array.new}
+    copy.each do |array|
+      array.each do |ele|
+        @store[ele.hash % (self.count * 2)] << ele
+      end
+    end
   end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    return @store[num.hash % self.num_buckets]
   end
 end
