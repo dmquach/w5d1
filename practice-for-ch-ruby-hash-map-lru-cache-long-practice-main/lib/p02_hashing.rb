9e9b5357802e7df1ea4da9ba0ed087aad632ac2b
Dylan <dylanquach@gmail.com>
2023-08-14 16:10:22 -0700

    Add hashing.rb

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    arr = []
+    self.each.with_index do |val, index|
+      arr << (val << index).to_s(2).to_i
+    end
+    arr.sum
   end
 end
 
 class String
   def hash
+    arr = self.split("")
+    new_arr = []
+    arr.each.with_index do |char, index|
+      new_arr << char.ord * (index + 4)
+    end
+
+    new_arr.join("").to_i
   end
 end
 
@@ -16,6 +28,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    arr = []
+    self.each do |key, value|
+      arr << key.hash * value.hash
+    end
+    arr.sum
   end
 end
