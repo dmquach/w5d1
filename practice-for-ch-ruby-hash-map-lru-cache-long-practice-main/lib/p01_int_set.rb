8bd9af8f6a6d06c399bd8c268aa8c52307a93c30
David <2018dpollack@gmail.com>
2023-08-14 14:23:51 -0700

    finish resizingIntSet

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -66,12 +66,23 @@ class ResizingIntSet
   end
 
   def insert(num)
+    self.resize! if self.count == num_buckets
+    unless self[num].include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    return true if self[num].include?(num)
+    false
   end
 
   private
@@ -81,9 +92,17 @@ class ResizingIntSet
   end
 
   def resize!
+    copy = @store.dup
+    @store = Array.new(@count * 2) {Array.new}
+    copy.each do |array|
+      array.each do |ele|
+        @store[ele % (self.count * 2)] << ele
+      end
+    end
   end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    return @store[num % self.num_buckets]
   end
 end
