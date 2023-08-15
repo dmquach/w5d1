82f0271f8ee4e4f4784db020f923967ac2073a30
Dylan <dylanquach@gmail.com>
2023-08-14 17:35:09 -0700

    Add HashMap

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p05_hash_map.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p05_hash_map.rb
@@ -1,4 +1,5 @@
 require_relative 'p04_linked_list'
+include Enumerable
 
 class HashMap
   attr_accessor :count
@@ -9,18 +10,40 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+      self.resize! if self.count == num_buckets
+      if !bucket(key).include?(key)
+        bucket(key).append(key, val)
+        @count += 1
+      else
+        bucket(key).update(key, val)
+      end
   end
 
   def get(key)
+    bucket(key).each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def delete(key)
+    @count -= 1
+    bucket(key).remove(key)
   end
 
   def each
+    @store.each do |list|
+     # yield nil if list.empty?
+      current = list.first
+      while current.key != nil
+        yield current
+        current = current.next
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -30,7 +53,7 @@ class HashMap
   #   end
   #   "{\n" + pairs.join(",\n") + "\n}"
   # end
-  
+
   alias_method :[], :get
   alias_method :[]=, :set
 
@@ -41,9 +64,17 @@ class HashMap
   end
 
   def resize!
+    copy = @store.dup
+    @store = Array.new(@count * 2) { LinkedList.new }
+    copy.each do |list|
+      list.each do |node|
+        @store[bucket(node.key)].append(node.key, node.val)
+      end
+    end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    return @store[key.hash % self.num_buckets]
   end
 end
